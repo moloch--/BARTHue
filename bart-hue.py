70962b8715fa1343bd53eeb0105338b42fb085a1
moloch <moloch38@gmail.com>
2015-04-19 14:34:52 -0700

    Switched to logging and evt loop

--- bart-hue.py
+++ bart-hue.py
@@ -7,6 +7,7 @@ This program adjusts your Hue lights based on the BART schedule
 
 import time
 import pyhue
+import logging
 import argparse
 
 from datetime import timedelta
@@ -30,31 +31,46 @@ def get_color_for_etd(etd):
     colors = Converter()
     if timedelta(minutes=10) <= etd:
         return colors.rgbToCIE1931(*GREEN)
-    elif timedelta(minutes=7) <= etd:
+    elif timedelta(minutes=6) <= etd:
         return colors.rgbToCIE1931(*BLUE)
     else:
         return colors.rgbToCIE1931(*RED)
 
 
+def bart_hue_loop(bart, station_name, destination, light, poll_interval):
+    ''' Main loop forever, and ever '''
+    while True:
+        try:
+            departure = bart[station_name][destination]
+            if departure is not None and 0 < len(departure.trains):
+                light.on = True
+                light.xy = get_color_for_etd(departure.trains[0].minutes)
+            else:
+                light.on = False
+            logging.info("Succesfully updated state")
+        except KeyboardInterrupt:
+            raise
+        except:
+            logging.exception("Error while updating state")
+        finally:
+            time.sleep(float(poll_interval))
+
+
 def bart_hue(station_name, destination, bridge_ip,
              light_name, poll_interval=30.0):
-    ''' Adjusts a Hue light based on a train schedule '''
-    print("BARTHue is starting up ...")
+    ''' Grab all the objects we need and start the loop '''
+    logging.info("BARTHue is starting up ...")
     bart = BART(BART_API_KEY)
     bridge = pyhue.Bridge(bridge_ip, HUE_USERNAME)
     light = next(light for light in bridge.lights if light.name == light_name)
-    while True:
-        departure = bart[station_name][destination]
-        if departure is not None and 0 < len(departure.trains):
-            light.on = True
-            light.xy = get_color_for_etd(departure.trains[0].minutes)
-        else:
-            light.on = False
-        print("Updated state, sleeping...")
-        time.sleep(float(poll_interval))
+    bart_hue_loop(bart, station_name, destination, light, poll_interval)
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(format="[%(levelname)s] %(asctime)s: %(message)s",
+                        level=logging.DEBUG)
+
     parser = argparse.ArgumentParser(
         description='Changes your Hue light(s) based on the BART schedule',
     )
