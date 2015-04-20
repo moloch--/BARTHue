2fbbd874afd941cdcdf9c279e6d4b06a0d824670
moloch <moloch38@gmail.com>
2015-04-20 15:24:52 -0700

    Added error counts, forking and more verbose logging

--- bart-hue.py
+++ bart-hue.py
@@ -5,6 +5,7 @@ This program adjusts your Hue lights based on the BART schedule
 
 '''
 
+import os
 import time
 import pyhue
 import logging
@@ -16,6 +17,7 @@ from BARTpy import BART
 
 HUE_USERNAME = 'newdeveloper'
 BART_API_KEY = 'ZJVD-UYTM-IKBQ-DT35'
+MAX_ERRORS = 50
 
 # RGB Color Tuples
 RED = (255, 0, 0)
@@ -39,6 +41,8 @@ def get_color_for_etd(etd):
 
 def bart_hue_loop(bart, station_name, destination, light, poll_interval):
     ''' Main loop forever, and ever '''
+    logging.info("Starting main loop")
+    errors = 0
     while True:
         try:
             departure = bart[station_name][destination]
@@ -51,25 +55,51 @@ def bart_hue_loop(bart, station_name, destination, light, poll_interval):
         except KeyboardInterrupt:
             raise
         except:
-            logging.exception("Error while updating state")
+            errors += 1
+            logging.exception("Error #%d while updating state" % errors)
+            if MAX_ERRORS <= errors:
+                logging.fatal("Reached maxium error count")
+                os._exit(-1)
         finally:
             time.sleep(float(poll_interval))
 
 
 def bart_hue(station_name, destination, bridge_ip,
-             light_name, poll_interval=30.0):
+             light_name, poll_interval):
     ''' Grab all the objects we need and start the loop '''
-    logging.info("BARTHue is starting up ...")
+    logging.info("BARTHue is starting up (pid: %s) ..." % os.getpid())
     bart = BART(BART_API_KEY)
+    logging.info("Connecting to Hue bridge at: %s" % bridge_ip)
     bridge = pyhue.Bridge(bridge_ip, HUE_USERNAME)
-    light = next(light for light in bridge.lights if light.name == light_name)
-    bart_hue_loop(bart, station_name, destination, light, poll_interval)
+    lights = filter(lambda light: light.name == light_name, bridge.lights)
+    if len(lights):
+        logging.info("Found light with name '%s'" % light_name)
+        bart_hue_loop(bart, station_name, destination,
+                      lights[0], poll_interval)
+    else:
+        logging.fatal("Could not find light '%s'" % light_name)
+        os._exit(1)
 
+
+def _main(args):
+    '''
+    Handles various administrivia then strarts the main loop
+    '''
+    if args.fork:
+        pid = os.fork()
+        if pid:
+            print("[*] Forking into background %s" % pid)
+            os._exit(0)
+    # Setup logging
+    logging.basicConfig(format="[%(levelname)s] %(asctime)s: %(message)s",
+                        filename=args.log_filename,
+                        level=logging.DEBUG)
+
+    # Start the main loop
+    bart_hue(args.station_name, args.destination, args.bridge_ip,
+             args.light_name, args.poll_interval)
 
 if __name__ == '__main__':
-
-    logging.basicConfig(format="[%(levelname)s] %(asctime)s: %(message)s",
-                        level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(
         description='Changes your Hue light(s) based on the BART schedule',
@@ -93,9 +123,17 @@ if __name__ == '__main__':
     parser.add_argument('--poll', '-p',
                         help='the polling interval',
                         dest='poll_interval',
-                        default=30.0,
+                        default=15.0,
                         type=float)
+    parser.add_argument('--fork', '-f',
+                        help='fork the process into the background',
+                        action='store_true',
+                        dest='fork')
+    parser.add_argument('--log-filename', '-log',
+                        help='save logging information to file',
+                        dest='log_filename',
+                        default="barthue.log")
     try:
-        bart_hue(**vars(parser.parse_args()))
+        _main(parser.parse_args())
     except KeyboardInterrupt:
         print("User stop, have a nice day!")
